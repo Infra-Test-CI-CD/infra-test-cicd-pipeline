455a313601bec6f9f43d713d6612d997d4b9d200
manojsk-4777 <manoj.sk@applogiq.org>
2025-09-30 14:56:27 +0530

    remote

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>About Page</h1>
+      <p>This is a basic Next.js static page.</p>
+      <a href="/">Back to Home</a>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Next.js + React App</h1>
+      <p>This is the home page.</p>
+      <a href="/about">Go to About Page</a>
+    </div>
+  )
+}
